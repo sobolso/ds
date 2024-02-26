0397850aef67218d145c6a556f445afd41d748ed
Sofya Sobol <sophie99@mail.ru>
2024-02-26 14:16:49 +0300

    first commit

---
+++ guess-number-task/game_v2.py
@@ -0,0 +1,55 @@
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
+
+import numpy as np
+
+
+def game_core(number: int = 1) -> int:
+    """Сначала устанавливаем любое random число, потом уменьшаем
+    или увеличиваем его в зависимости от того, больше оно или меньше нужного. 
+    Функция принимает загаданное число и возвращает число попыток.
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+
+    predict = np.random.randint(1, 101)  # загаданное число
+    
+    while number != predict:
+        count += 1
+        if number > predict:   
+            predict += 5  # если искомое число больше загаданного, то прибавляем 5
+        elif number < predict:
+            predict -= 2  # если искомое число меньше загаданного, то вычитаем 2
+
+    return count
+
+
+def score_game(game_core) -> int:
+    """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        game_core([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = [] # список для сохранения количества попыток
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(game_core(number))
+
+    score = int(np.mean(count_ls))  # находим среднее количество попыток
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+# RUN
+if __name__ == "__main__":
+    score_game(game_core)
